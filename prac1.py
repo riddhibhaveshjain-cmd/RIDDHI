89a975e414a253814b43835670cd315eed02f322
riddhibhaveshjain-cmd <riddhibhaveshjain@gmail.com>
2026-04-28 17:38:44 +0530

    addidition part added

---
+++ prac1.py
@@ -0,0 +1,13 @@
+def  square_numbers(nums):
+    return[x**2 for x in nums]
+print(square_numbers([1,2,3,4]))
+
+list=("apple","mango","banana")
+def sort_strings(list):
+    return sorted(list)
+print(sort_strings(list))
+
+list=[1,2,3,4,5,6,-1,-2,-3,9,-4]
+def sum_positive(nums):
+    return list > 0
+print(sum_positive(list))
