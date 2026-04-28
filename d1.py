914e5ea23d9379260f866d08a19b303f9a2da5c6
riddhibhaveshjain-cmd <riddhibhaveshjain@gmail.com>
2026-04-28 08:24:00 +0000

    day1completed

---
+++ d1.py
@@ -0,0 +1,28 @@
+#w= 2
+#l = 2
+#AOR = w*l
+#print(AOR)
+
+#name = input("enter the name:")
+#age= input("enter the age:")
+#print(f"Hello {name}! You are {age} years old.")
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    #print("Even")
+#else:
+   # print("odd")
+
+#l = 2
+#AOR = w*l
+#print(AOR)
+
+#name = input("enter the name:")
+#age= input("enter the age:")
+#print(f"Hello {name}! You are {age} years old.")
+
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    #print("Even")
+#else:
+   # print("odd")
