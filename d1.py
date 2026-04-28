d2a7bebdc1e326800e66a54b43376b12138f8b9a
riddhibhaveshjain-cmd <riddhibhaveshjain@gmail.com>
2026-04-28 08:25:25 +0000

    day1complted

--- d1.py
+++ d1.py
@@ -7,22 +7,57 @@
 #age= input("enter the age:")
 #print(f"Hello {name}! You are {age} years old.")
 
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    #print("Even")
+#num = int(input("Enter a number: "))
+#if num % 2 == 0:
+   # print("Even number")
 #else:
-   # print("odd")
+    #print("Odd number") 
 
-#l = 2
-#AOR = w*l
-#print(AOR)
+#numbers = [12, 45, 7, 23, 89]
 
-#name = input("enter the name:")
-#age= input("enter the age:")
-#print(f"Hello {name}! You are {age} years old.")
+#largest = max(numbers)
+#smallest = min(numbers)
 
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    #print("Even")
+#print(largest)
+#print(smallest)
+
+#def is_palindrome(s):
+       # return s == s[::-1]
+
+#text = input("Enter a number: ")
+#if is_palindrome(text):
+       # print("Palindrome")
 #else:
-   # print("odd")
+    #print("Not a palindrome")
+
+#p = 6
+#r = 2
+#t = 1
+
+#ci = p * (1 + r/100) ** t
+#print(ci)
+
+#list = [1,2,3,4,5,6,7,8,9]
+#for num in list:
+    #if num %2==0:  
+       # print(num)
+
+#sentence = input("Enter a sentence: ")
+#words = sentence.split()
+#word_count = len(words)
+
+#print(f"Number of words: {word_count}")
+
+a = 3
+b = 4
+
+print(f"Before swap: a = {a}, b = {b}")
+
+a, b = b, a
+
+print(f"After swap:  a = {a}, b = {b}")
+
+
+
+
+
