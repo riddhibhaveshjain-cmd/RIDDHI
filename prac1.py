57da76c3a0fdf80a5eff7a645ee33b918226f92d
riddhibhaveshjain-cmd <riddhibhaveshjain@gmail.com>
2026-04-28 17:41:08 +0530

    addidition part added

--- prac1.py
+++ prac1.py
@@ -7,7 +7,14 @@ def sort_strings(list):
     return sorted(list)
 print(sort_strings(list))
 
-list=[1,2,3,4,5,6,-1,-2,-3,9,-4]
-def sum_positive(nums):
-    return list > 0
-print(sum_positive(list))
+# list=[1,2,3,4,5,6,-1,-2,-3,9,-4]
+# def sum_positive(nums):
+#     return list > 0
+# print(sum_positive(list))
+
+# l1=[1,2,3,4,5,6]
+# l2=[2,4,6,8,10,12]
+
+k=int(input("enter the value":))
+for i
+
