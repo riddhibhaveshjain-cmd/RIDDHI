7a5a193d9d87f403ea85698b8afb6b68825974eb
riddhibhaveshjain-cmd <riddhibhaveshjain@gmail.com>
2026-05-06 17:26:19 +0530

    with statement

--- FILEio.py
+++ FILEio.py
@@ -1,4 +1,25 @@
-f = open("poems.txt","r")
-text = f.read()
-print(text)
+# f = open("poems.txt","r")
+# text = f.read()
+# print(text)
+# f.close()
+
+# f = open("greeting.txt" , "a")
+# f.write("Have a nice day\n")
+# f.close()
+
+# with open('greeting.txt','r') as f :
+#     a = f.read()
+# with open('greeting.txt','w') as f :
+#     a = f.write("me")
+# with open('greeting.txt','a') as f :
+#     a = f.write("hello !")
+# print(a)
+
+with open("poems.txt", "r") as f:
+    a = f.read()
+print(a)
+
+f = open("copy_poems.txt" , "w")
+f.write(a)
 f.close()
+
